329b0613e4901bbd583764430383ad66a8922ff2
Santosh Kumar <santoshsah017@gmail.com>
2024-11-17 18:08:32 +0530

    General example

---
+++ typescript-tutorial/build/js/test.js
@@ -0,0 +1,14 @@
+"use strict";
+let myName = "dave";
+myName = 'anil';
+let meaningoflife = 43;
+meaningoflife = 42;
+let album;
+album = "van halen";
+album = 30;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ typescript-tutorial/src/test.ts
@@ -0,0 +1,21 @@
+let myName:string = "dave";
+
+myName = 'anil';
+
+let meaningoflife:number = 43;
+
+meaningoflife = 42
+
+let album:string | number;
+
+album = "van halen";
+album = 30;
+
+const sum = (a:number, b:string)=>{
+    return a+b;
+}
+
+let postId: string | number 
+let isActive: number | boolean
+
+let re:RegExp = /\w+/g
